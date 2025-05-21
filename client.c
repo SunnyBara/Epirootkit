fcd843c5e09c9d10e4bc51427075d40203d1d4fc
sunny.soyer@epita.fr <sunny.soyer@epita.fr>
2025-05-21 10:44:08 +0200

    Add serveur thread and connection handshake

--- client.c
+++ client.c
@@ -6,17 +6,50 @@
 
 #define PORT 4444 
 
+
+int find_size(int n)
+{
+    int size = 0;
+    while (n / 10 > 0)
+    {
+        n /= 10;
+        size++;
+    }
+    return size;
+}
+
+char *my_itoa(int value, char *s)
+{
+    if (value == 0)
+    {
+        s[0] = '0';
+    }
+    int i = 0;
+    if (value < 0)
+    {
+        value *= -1;
+        s[i] = '-';
+        i++;
+    }
+    int size = find_size(value);
+    size += i;
+    s[size + 1] = '\0';
+    while (value > 0)
+    {
+        s[size] = value % 10 + '0';
+        value /= 10;
+        size--;
+    }
+    return s;
+}
+
 int main(int argc, char *argv[]) {
-    // verif du nombre d'arguments (IP + nom de fichier)
     if (argc != 3) {
         printf("Usage: %s <IP> <filename>\n", argv[0]);
         return 1;
     }
 
-    // socket tcp
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    // config addresse serveur
     struct sockaddr_in server;
     server.sin_family = AF_INET;          // adresses IPv4
     server.sin_port = htons(PORT);        // port converti en format réseau
@@ -28,18 +61,34 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // ouvre le fichier en mode binaire 
-    FILE *f = fopen(argv[2], "rb");
 
-    char buffer[1024];
-    int n;
+    // // ouvre le fichier en mode binaire 
+    // FILE *f = fopen(argv[2], "rb");
 
-    // lecture du fichier par blocs de 1024 octets et envoi au serveur
-    while ((n = fread(buffer, 1, 1024, f)) > 0) {
-        send(sock, buffer, n, 0);  // envoyer les données lues
-    }
+    // char buffer[1024];
+    // int n;
 
-    fclose(f);
+    // // lecture du fichier par blocs de 1024 octets et envoi au serveur
+    // while ((n = fread(buffer, 1, 1024, f)) > 0) {
+    //     send(sock, buffer, n, 0);  // envoyer les données lues
+    // }
+    char *inforecv = malloc(6);
+    recv(sock, inforecv, 6,0);
+    printf("rcv %s\n",inforecv);
+    int key = atoi(inforecv);
+    key -= 1;
+    key /= 100;
+    char parsed[4];
+    char pass[34] = "base_pass";
+    my_itoa(key, parsed);
+    strcat(pass,parsed);
+    send(sock, pass, strlen(pass),0);
+    //fclose(f);
+    char receive[101];
+    int len = recv(sock, receive, 100,0);
+    receive[len] = '\0'; 
+    printf("response %s\n",receive);
+
     close(sock);
 
     return 0;
--- server.c
+++ server.c
@@ -1,42 +1,171 @@
+#include <arpa/inet.h>
+#include <math.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 4444 
 
-int main() {
-    int server_fd, client_fd;          // descipteurs de sockets serveur et client
-    char buffer[1024];                 // buffer pour stocker les données reçues
-    struct sockaddr_in addr;           // structure pour l'adresse du serveur/client
-    socklen_t addrlen = sizeof(addr);  
+typedef struct Server {
+    int server_fd;
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+} s_server;
 
-    // socket TCP IPv4
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+int find_size(int n)
+{
+    int size = 0;
+    while (n / 10 > 0)
+    {
+        n /= 10;
+        size++;
+    }
+    return size;
+}
 
-    // Configuration de l'adresse du serveur
-    addr.sin_family = AF_INET;          //  IPv4
-    addr.sin_addr.s_addr = INADDR_ANY; // accepte les connexions sur toutes les interfaces réseau
-    addr.sin_port = htons(PORT);        
+char *my_itoa(int value, char *s)
+{
+    if (value == 0)
+    {
+        s[0] = '0';
+    }
+    int i = 0;
+    if (value < 0)
+    {
+        value *= -1;
+        s[i] = '-';
+        i++;
+    }
+    int size = find_size(value);
+    size += i;
+    s[size + 1] = '\0';
+    while (value > 0)
+    {
+        s[size] = value % 10 + '0';
+        value /= 10;
+        size--;
+    }
+    return s;
+}
 
-    // on associe la socket à l'adresse et au port spécifiés
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(server_fd, 1);
 
-    // on accepte la connexion entrante, création d'une nouvelle socket client
-    client_fd = accept(server_fd, (struct sockaddr*)&addr, &addrlen);
-    FILE *f = fopen("received_file", "wb");
+void genere_password(char *pass, char *key)
+{
+    char parsed[12];
+    int created = rand() % 162;
+    my_itoa(created, parsed);
+    strcat(pass, parsed);
+    int manipulated = (created * 100 ) + 1;
+    my_itoa(manipulated, key);
+    return;
+}
 
-    int n;
-    // on lis les données reçues depuis la socket client, puis on écris dans le fichier
-    while ((n = read(client_fd, buffer, 1024)) > 0) {
-        fwrite(buffer, 1, n, f);
+int handshake(int socket_client)
+{
+    char pass[32] = "base_pass";
+    char key[16];
+    genere_password(pass, key);
+    send(socket_client, x, strlen(key), 0);
+    char try[64] = {0};
+    int r = recv(socket_client, try, sizeof(try) - 1, 0);
+    if (r <= 0) {
+        perror("recv");
+        return -1;
     }
 
-    fclose(f);
-    close(client_fd);
-    close(server_fd);
+    try[r] = '\0';
+    printf("Recu du client : %s\n", try);
+    printf("Mot de passe attendu : %s\n", pass);
+
+    if (strcmp(pass, try) == 0)
+    {
+        send(socket_client, "authenticated", strlen("authenticated"), 0);
+        return 1;
+
+    }
+    send(socket_client, "perdu aller salut mdr", strlen("perdu aller salut mdr"), 0);
+    return 0;
+}
+
+void *manage_client(void *arg)
+{
+    int *client_fd;
+    client_fd =  (int *) arg;
+    if (handshake(*client_fd) == 1)
+    {
+        printf("suite de l implementation\n");
+    }
+    close(*client_fd);
+    return NULL;
+
+
+}
+
+void *client_connect(void *arg)
+{
+    s_server *server = (s_server *) arg; 
+    if (listen(server->server_fd, 1) < 0) {
+            perror("listen");
+            close(server->server_fd);
+            exit(EXIT_FAILURE);
+        }
+    while(1)
+    {
+
+        int *client_fd = malloc(sizeof(int));
+        *client_fd = accept(server->server_fd, (struct sockaddr*)&server->addr, &server->addrlen);
+        if (*client_fd < 0) {
+            perror("accept");
+            close(server->server_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_t client_thread;
+        pthread_create(&client_thread, NULL, manage_client, client_fd);
+    }
+    return NULL;
+}
+
+
+int main() {
+    char buffer[1024];                 // buffer pour stocker les données reçues
+    struct sockaddr_in addr;          // structure pour l'adresse du serveur/client
+    s_server *server = malloc(sizeof(s_server)); 
+    server->server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server->server_fd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    memset(&server->addr, 0, sizeof(server->addr));
+    server->addr = addr;
+    server->addr.sin_family = AF_INET;
+    server->addr.sin_addr.s_addr = INADDR_ANY;
+    server->addr.sin_port = htons(PORT);
+    server->addrlen = sizeof(server->addr);
+
+    if (bind(server->server_fd, (struct sockaddr*)&server->addr, server->addrlen) < 0) {
+        perror("bind");
+        close(server->server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // FILE *f = fopen("received_file", "wb");
+
+    // int n;
+    // // on lis les données reçues depuis la socket client, puis on écris dans le fichier
+    // while ((n = read(client_fd, buffer, 1024)) > 0) {
+    //     fwrite(buffer, 1, n, f);
+    // }
+
+    //fclose(f);
+    pthread_t client_connector;
+    pthread_create(&client_connector, NULL, client_connect, server);
+    pthread_join(client_connector, NULL);
+    close(server->server_fd);
+    free(server);
 
     return 0;
 }
